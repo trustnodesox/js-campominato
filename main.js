9e894fcecc05ab0a8e4d2e70226abeb8470a123e
Lorenzo De Sossi <lollodesossi@gmail.com>
2021-03-09 20:52:14 +0100

    funzione e array

--- main.js
+++ main.js
@@ -1,17 +1,35 @@
 //Genero 16 numeri casuali tra 1 e 100
-for (var i = 0; i < 16; i++) {
-  i = Math.floor(Math.random() * 101)
-  console.log(i);
+//for (var i = 0; i < 16; i++) {
+
+//0}
+
+//creare una function che genera un numero casuale
+function numerorandom (){
+   (Math.floor(Math.random() * 101))
+   return (Math.floor(Math.random() * 101))
 }
 
+//salvare il risultato in una variabile esterna dove richiamo la funzione
+var numeroF = numerorandom;
+
+//stampo la variabile
+console.log(numeroF);
+
+//Creo un array dove verranno pushati 16 numeri
+var numeri = [];
+
+//creare un while dove : condizione: array.length<16
+while (numeri.length<16) {
+  numeri.push(numeroF)
+}
+
+//stampo tutto l'array
+console.log(numeri);
 
 
 //Chiedo all'utente di inserire un numero
 var utente = prompt("Inserisci un numero");
 
-//Creo un array dove verra
-var numeri = [];
-
 //for (var i = 0; i < 100; i++) {
-//  numeri.push(utente)[i]
-//}
+  //  numeri.push(utente)[i]
+  //}
