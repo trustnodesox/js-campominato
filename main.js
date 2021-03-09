f304b979ab5cb4b34831da722c5b620f7cdf2058
Lorenzo De Sossi <lollodesossi@gmail.com>
2021-03-09 16:38:17 +0100

    inizio

---
+++ main.js
@@ -0,0 +1,12 @@
+
+//Chiedo all'utente di inserire un numero
+var utente = prompt("Inserisci un numero");
+
+
+var numeri = [];
+
+for (var i = 0; i < 100; i++) {
+  numeri.push(utente)[i]
+}
+console.log(utente);
+console.log(numeri);
