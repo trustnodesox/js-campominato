e3c0ca8a74165c6b0143678dcd3bf30d5f6e4b8d
Lorenzo De Sossi <lollodesossi@gmail.com>
2021-03-09 16:50:06 +0100

    ciclo for con math.random

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
+//Genero 16 numeri casuali tra 1 e 100
+for (var i = 0; i < 16; i++) {
+  i = Math.floor(Math.random() * 101)
+  console.log(i);
+}
+
+
 
 //Chiedo all'utente di inserire un numero
 var utente = prompt("Inserisci un numero");
 
-
+//Creo un array dove verra
 var numeri = [];
 
-for (var i = 0; i < 100; i++) {
-  numeri.push(utente)[i]
-}
-console.log(utente);
-console.log(numeri);
+//for (var i = 0; i < 100; i++) {
+//  numeri.push(utente)[i]
+//}
